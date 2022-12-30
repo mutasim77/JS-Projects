95d9c2389f4836cf1f4f7e33210f9125bece47d8
Mutasim <qmutasim77@gmail.com>
2022-12-30 21:30:45 +0600

    Add Login

---
+++ LinearGradient/js/script.js
@@ -0,0 +1,29 @@
+let table = document.querySelector('.table');
+let dropDown = document.querySelector('select');
+let inputs = document.querySelectorAll('input');
+let code = document.querySelector('#code');
+let message = document.querySelector('.message');
+
+//! When we choose color
+inputs.forEach(item => {
+    item.addEventListener('input', setColors);
+})
+
+//! When we choose oreontation
+dropDown.addEventListener('change', setColors);
+
+//! Function that sets colors and oreontation
+function setColors() {
+    let color1 = inputs[0].value;
+    let color2 = inputs[1].value;
+    let oreontation = dropDown.value;
+    code.innerText = table.style.background = `linear-gradient(${oreontation}, ${color1}, ${color2})`;
+    code.style.color = color1
+}
+
+//! Copy the code
+document.querySelector('.code').addEventListener('click', () => {
+    navigator.clipboard.writeText(text).then(function () {
+        // message.
+    })
+})
