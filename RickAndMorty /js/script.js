d98a0f1017d12fd0e75ddb0de6c6189ceeac6e12
Mutasim <qmutasim77@gmail.com>
2023-02-11 06:48:19 +0600

    Updated

--- RickAndMorty /js/script.js
+++ RickAndMorty /js/script.js
@@ -1,5 +1,25 @@
 import RickMortyService from "./rickMortyService.js";
 
 const api = new RickMortyService();
-console.log(api.getAllCharacters().then(item => console.log(item)));
 
+api.getAllCharacters()
+    .then(item => {
+        showAllCharacters(item);
+    })
+
+function showAllCharacters(arr) {
+    arr.forEach(item => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML =
+            `
+            <img src="${item.image}"
+                alt="rick">
+            <div class="flex alig-center justify-between pr-2">
+                <h3 class="name">${item.name}</h3>
+                <div class="like"><i class="fa fa-heart" aria-hidden="true"></i></div>
+            </div>
+        `;
+        document.querySelector('#cards').append(card);
+    })
+}
