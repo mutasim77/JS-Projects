100d4a2e22987412d0e6d591134b2844a46c7585
Mutasim <qmutasim77@gmail.com>
2023-03-03 12:12:07 +0600

    Updated

--- GitHubUsers/js/script.js
+++ GitHubUsers/js/script.js
@@ -1,10 +1,22 @@
+const API = 'https://api.github.com/users';
+
+//! Fetch the Data
+const fetchData = async (url, username) => {
+    const res = await fetch(`${url}/${username}`);
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+    return await res.json();
+}
+
+console.log(fetchData(API, 'mutasim77'));
+
 //! Dark Mode
 const buttonDark = document.querySelector('.dark-mode button');
 buttonDark.addEventListener('click', () => {
     buttonDark.classList.toggle('dark');
     document.body.classList.toggle('dark');
 });
-
 
 //! Search Button
 const buttonSearch = document.querySelector('[data-action="search"]');
